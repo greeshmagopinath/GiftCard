6ab3c3def0ccff47809364367b15ba54241a34c1
Greeshma Gopinath <grgopina@microsoft.com>
2018-11-18 20:03:32 -0800

    making utlity a module

--- utility.py
+++ utility.py
@@ -1,18 +1,16 @@
-class Utility(object):
-    """ Class with static helper methods"""
-
-    @staticmethod
-    def read_file(filename):
-        '''
-        Reads a file with keys and values and converts to an array
-        :param filename: name of the file
-        todo: input validation
-        :return:  array containing keys and values
-        '''
-        content = open(filename)
-        lines = content.readlines()
-        arr = []
-        for line in lines:
-            item,value = line.strip('\n').split(',')
-            arr.append([item,int(value)])
-        return arr
+def read_file(filename):
+    '''
+    Reads a file with keys and values and converts to an array
+    :param filename: name of the file
+    todo:
+     * input validation
+     * create model objects for item, value
+    :return:  array containing keys and values
+    '''
+    content = open(filename)
+    lines = content.readlines()
+    arr = []
+    for line in lines:
+        item,value = line.strip('\n').split(',')
+        arr.append([item,int(value)])
+    return arr
